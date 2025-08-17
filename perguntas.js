b5c74fd1aba05ec8f81eebf14ae1d86b8c195618
GitMatheusGit <alves.matheus12@escola.pr.gov.br>
2025-08-17 02:36:15 -0300

    pergutas eztras

--- perguntas.js
+++ perguntas.js
@@ -11,7 +11,38 @@ criaCartao(
 )
 
 criaCartao(
-    'JOJO?',
+    'JOJO',
     'O personagem mais famoso de jojo?',
     'Jotaro'
 )
+
+criaCartao(
+    'Minecraft',
+    'Qual o nome do boss do the end?',
+    'Ender Dragon'
+)
+
+criaCartao(
+    'Roblox',
+    'Qual o jogo de anime mais jogado do roblox?',
+    'Blox Fruits'
+)
+
+criaCartao(
+    'LOL',
+    'Qual o champion mais recente?',
+    'Yunara'
+)
+
+criaCartao(
+    'Hollow knight',
+    'Qual o nome do boss final?',
+    'Radiancia'
+)
+
+criaCartao(
+    'One piece',
+    'Qual o nome do protagonista?',
+    'Luffy'
+)
+
